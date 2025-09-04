b59f018f91018c09447669b4f05139643f0fc8d7
PitRella <serezha.sliva@gmail.com>
2025-09-05 01:11:54 +0300

    feat(authors) - refactor AuthorService to use repository pattern and complete create_author endpoint implementation

--- src/authors/router.py
+++ src/authors/router.py
@@ -1,12 +1,17 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 
 from src.authors.schemas import CreateAuthorRequestSchema
+from src.authors.service import AuthorService
+from src.base.dependencies import get_service
 
 author_router = APIRouter(prefix='/author', tags=['author'])
 
 
-@author_router.get('/', description='Create a new author.')
+@author_router.post('/', description='Create a new author.')
 async def create_author(
         author_schema: CreateAuthorRequestSchema,
+        service: Annotated[AuthorService, Depends(get_service(AuthorService))],
 ) -> None:
-    pass
+    await service.create_author(author_schema)
--- src/authors/service.py
+++ src/authors/service.py
@@ -1,9 +1,8 @@
 from datetime import datetime as dt, timezone
-
+from src.authors.repositories import AuthorRepository
 from src.authors.schemas import CreateAuthorRequestSchema
 from src.base.services import BaseService
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import text
 
 
 class AuthorService(BaseService):
@@ -11,28 +10,14 @@ class AuthorService(BaseService):
             self,
             db_session: AsyncSession
     ) -> None:
-        super().__init__(db_session)
+        super().__init__(db_session, repo=AuthorRepository(db_session))
 
-    async def create_author(self, author_schema: CreateAuthorRequestSchema) -> None:
-        # TODO: Add password hash
-        sql = text(
-            """
-            INSERT INTO authors (email, password, name, biography,
-                                 birth_year, nationality, created_at)
-            VALUES (:email, :password, :name, :biography, :birth_year,
-                    :nationality, :created_at) RETURNING id
-            """
-        )
-        params = {
-            "email": author_schema.email,
-            "password": author_schema.password,  # TODO: Add password hash
-            "name": author_schema.name,
-            "biography": author_schema.biography,
-            "birth_year": author_schema.birth_year,
-            "nationality": author_schema.nationality,
-            "created_at": dt.now(timezone.utc)
-        }
-        async with self._session.begin():
-            result = await self._session.execute(sql, params)
-            author_id = result.scalar_one()
+    async def create_author(
+            self,
+            author_schema: CreateAuthorRequestSchema
+    ) -> int:
+        author_data = author_schema.model_dump()
+        # user_data['password'] = # TODO: Add password hash
+        author_data['created_at'] = dt.now(timezone.utc)
+        author_id: int = await self._repo.create_object(author_data)
         return author_id
