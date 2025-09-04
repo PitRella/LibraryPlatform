84cd12b75ec1d521cf8b46a9fe0eb55d3a347c8b
PitRella <serezha.sliva@gmail.com>
2025-09-05 00:55:47 +0300

    feat(authors) - implement create_author method with database insertion logic

--- src/authors/service.py
+++ src/authors/service.py
@@ -1,6 +1,9 @@
+from datetime import datetime as dt, timezone
+
 from src.authors.schemas import CreateAuthorRequestSchema
 from src.base.services import BaseService
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import text
 
 
 class AuthorService(BaseService):
@@ -10,5 +13,26 @@ class AuthorService(BaseService):
     ) -> None:
         super().__init__(db_session)
 
-    def create_author(self, author_schema: CreateAuthorRequestSchema) -> None:
-        raise NotImplementedError()
+    async def create_author(self, author_schema: CreateAuthorRequestSchema) -> None:
+        # TODO: Add password hash
+        sql = text(
+            """
+            INSERT INTO authors (email, password, name, biography,
+                                 birth_year, nationality, created_at)
+            VALUES (:email, :password, :name, :biography, :birth_year,
+                    :nationality, :created_at) RETURNING id
+            """
+        )
+        params = {
+            "email": author_schema.email,
+            "password": author_schema.password,  # TODO: Add password hash
+            "name": author_schema.name,
+            "biography": author_schema.biography,
+            "birth_year": author_schema.birth_year,
+            "nationality": author_schema.nationality,
+            "created_at": dt.now(timezone.utc)
+        }
+        async with self._session.begin():
+            result = await self._session.execute(sql, params)
+            author_id = result.scalar_one()
+        return author_id
