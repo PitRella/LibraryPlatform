479334aa4a8350e0d8e257ab313979af16266b23
PitRella <serezha.sliva@gmail.com>
2025-09-05 00:32:48 +0300

    feat(dependencies) - add generic service dependency injection factory for FastAPI

---
+++ src/base/dependencies.py
@@ -0,0 +1,19 @@
+from collections.abc import Callable
+from typing import Annotated, TypeVar
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.base.services import BaseService
+from src.database import get_db
+
+Service = TypeVar('Service', bound=BaseService)
+
+def get_service[Service](
+    service_type: type[Service],
+) -> Callable[[AsyncSession], Service]:
+
+    def _get_service(db: Annotated[AsyncSession, Depends(get_db)]) -> Service:
+        return service_type(db_session=db)  # type: ignore
+
+    return _get_service
